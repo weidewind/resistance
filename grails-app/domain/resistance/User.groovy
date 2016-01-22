b73d7b24621afcdf3287847c90fc7ec36001b669
weidewind <weidewind@gmail.com>
2016-01-22 17:20:47 +0300

    bootstrap pagination

--- grails-app/domain/resistance/User.groovy
+++ grails-app/domain/resistance/User.groovy
@@ -1,14 +1,13 @@
 package resistance
 
-class User
-{
-Long id
-String email
-String password
-String toString()
-{ "$email" }
-static constraints = {
- email(email:true)
- password(blank:false, password:true)
-}
+class User {
+	Long id
+	String email
+	String password
+	String toString()
+	{ "$email" }
+	static constraints = {
+		email(email:true)
+		password(blank:false, password:true)
+	}
 }
